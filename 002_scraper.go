4593f342c5d778b3712d90562a270b8800d687ea
Luca Albrecht <lal126594@iet-gibb.ch>
2022-03-24 16:30:00 +0100

    Added Simple GET Scraper

---
+++ 002_scraper.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+   "io/ioutil"
+   "log"
+   "net/http"
+)
+
+var reqUrl = "https://irefurb.ch/"
+
+func main() {
+   resp, err := http.Get(reqUrl)
+   if err != nil {
+      log.Fatalln(err)
+   }
+
+   body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+
+   sb := string(body)
+   log.Printf(sb)
+}
