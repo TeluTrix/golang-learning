d095a7555879db7fb4364342e46ea54a61cb0a17
Luca Albrecht <lal126594@iet-gibb.ch>
2022-03-24 16:34:29 +0100

    Updated Scraper and Added Post Request Sample

--- 002_scraper.go
+++ 002_scraper.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-   "io/ioutil"
-   "log"
-   "net/http"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 var reqUrl = "https://irefurb.ch/"
 
 func main() {
-   resp, err := http.Get(reqUrl)
-   if err != nil {
-      log.Fatalln(err)
-   }
+	resp, err := http.Get(reqUrl)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-   body, err := ioutil.ReadAll(resp.Body)
-   if err != nil {
-      log.Fatalln(err)
-   }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-   sb := string(body)
-   log.Printf(sb)
+	sb := string(body)
+	log.Printf(sb)
 }
